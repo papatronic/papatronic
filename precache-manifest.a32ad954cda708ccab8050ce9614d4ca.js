f31f55c394d018a27712e16a411388d58a4b467d
René Cecil Vidríales Trujillo <rene.vidriales@seistreinta.com>
2019-06-12 10:03:11 -0600

    Updates

---
+++ precache-manifest.a32ad954cda708ccab8050ce9614d4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ecca5939470c7b4ffbd00377885b8f",
+    "url": "/papatronic/index.html"
+  },
+  {
+    "revision": "39d46c356ae8966a79ac",
+    "url": "/papatronic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/papatronic/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "39d46c356ae8966a79ac",
+    "url": "/papatronic/static/js/main.e033a3e4.chunk.js"
+  },
+  {
+    "revision": "c252de006d5ce5e54e54",
+    "url": "/papatronic/static/js/runtime~main.30a36329.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/papatronic/static/media/logo.5d5d9eef.svg"
+  }
+]);
