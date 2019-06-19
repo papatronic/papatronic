6fd043cb47e189fddfafbd685e5f60ba572033d7
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-06-18 23:55:07 -0600

    Added basic body of components and routing for each of them, referencing issue #3

---
+++ src/routes/Predict.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Predict extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                Im the Predict component!
+            </div>
+        );
+    }
+}
+
+export default Predict;
