7095876f2d4738375787e2f6511732f439f79215
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-06-24 22:03:57 -0600

    #11 merged the color theme constants and the browser information values due to immutability accross the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { HashRouter, Switch } from 'react-router-dom';
-import { BrowserContext } from './contexts/BrowserContext';
+import { AppContext } from './contexts/AppContext';
 import BrowserInformation from './helpers/BrowserInformation'
 import ContextComponent from './components/ContextComponent';
 import Home from './routes/Home';
 import NotFound from './routes/NotFound';
 import Predict from './routes/Predict';
 import BrowserNotSupported from './routes/BrowserNotSupported';
+import ColorTheme from './constants/colors';
 import './App.css';
 
 function App() {
@@ -14,10 +15,10 @@ function App() {
   return (
     <HashRouter>
       <Switch>
-        <ContextComponent Component={Home} context={BrowserContext} value={{ version, completeVersion, browserName, supported }} exact path='/' />
-        <ContextComponent exact path='/predict' value={{ version, completeVersion, browserName, supported }} context={BrowserContext} Component={Predict} />
-        <ContextComponent exact path='/browser-not-supported' value={{ version, completeVersion, browserName, supported }} context={BrowserContext} Component={BrowserNotSupported} />
-        <ContextComponent Component={NotFound} value={{ version, completeVersion, browserName, supported }} context={BrowserContext} />
+        <ContextComponent Component={Home} context={AppContext} value={{ version, completeVersion, browserName, supported, colors: ColorTheme }} exact path='/' />
+        <ContextComponent exact path='/predict' value={{ version, completeVersion, browserName, supported, colors: ColorTheme }} context={AppContext} Component={Predict} />
+        <ContextComponent exact path='/browser-not-supported' value={{ version, completeVersion, browserName, supported, colors: ColorTheme }} context={AppContext} Component={BrowserNotSupported} />
+        <ContextComponent Component={NotFound} value={{ version, completeVersion, browserName, supported, colors: ColorTheme }} context={AppContext} />
       </Switch>
     </HashRouter>
   );
