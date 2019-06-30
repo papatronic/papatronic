c1dd8bc0784f1afb5817b9e6bfeccae8b8e8e2e7
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-06-29 18:58:16 -0600

    #6 removed svg logo and added styled component integration with not found route markup

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import { FlexContainer, FlexCard } from '../components/FlexContainers';
 import { AppContext } from '../contexts/AppContext';
 import { NFS } from '../constants/strings';
 import '../styles/not-found.css';
@@ -8,8 +9,8 @@ function NotFound() {
   const { supported, colors: { BLUE, GOLD } } = useContext(AppContext);
   return (
     !supported ? <Redirect to='/browser-not-supported' /> :
-    <div className="NFarentContainer">
-      <div className="NFCard">
+    <FlexContainer className="NFarentContainer">
+      <FlexCard className="NFCard">
         <div className="NFTitles" style={{color: BLUE}}>
           <div id="NFCode">{NFS.code}</div>
           <div id="NFTitle">{NFS.title}</div>
@@ -18,8 +19,8 @@ function NotFound() {
           <div id="NFBodyBody">{NFS.body}</div>
         </div>
         <Link className="NFButton" id="NFButtonStyle" to="/" style={{backgroundColor: GOLD}}>{NFS.buttonText}</Link>
-      </div>
-    </div>
+      </FlexCard>
+    </FlexContainer>
   );
 }
 
