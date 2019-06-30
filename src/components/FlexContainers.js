2687824c6d4ab49c0f5a8e7f230c383ca4f9aff0
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-06-29 18:56:06 -0600

    #6 added styled componnts for reusable containers with flexbox, added changes in index css file

---
+++ src/components/FlexContainers.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FlexCard = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
