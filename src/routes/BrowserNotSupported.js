79f4ebe077319018b2c72dd78a1311f86b171ef7
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-06-29 18:56:58 -0600

    #6 added browser not supported route markup and added asets for png

--- src/routes/BrowserNotSupported.js
+++ src/routes/BrowserNotSupported.js
@@ -1,10 +1,34 @@
 import React, { useContext } from 'react';
+import { FlexContainer, FlexCard } from '../components/FlexContainers';
 import { AppContext } from '../contexts/AppContext';
+import { BNSS } from '../constants/strings';
+import googleChromeLogo from '../assets/google_chrome_logo.png';
+import mozillaFirefoxLogo from '../assets/mozilla_firefox_logo.png';
+import appleSafariLogo from '../assets/apple_safari_logo.png';
+import '../styles/browser-not-supported.css';
 
 function BrowserNotSupported() {
-  const appContext = useContext(AppContext);
+  const { colors: { BLUE, GOLD } } = useContext(AppContext);
   return (
-    <div>Hello! Im the BrowserNotSupported component!</div>
+    <FlexContainer className="BNSParentContainer">
+      <FlexCard className="BNSCard">
+        <div style={{color: BLUE}} className="BNSTitle">
+          <h3>{BNSS.title}</h3>
+        </div>
+        <div className="BNSBody">
+          <p>
+            Tu navegador no es compatible con las tecnologías que utilizamos. 
+            Para la correcta visualización de éste sitio es necesario que nos 
+            visites desde <a href={BNSS.googleChromeDownloadLink} style={{color: GOLD}}>Google Chrome</a>, <a href={BNSS.mozillaFirefoxDownloadLink} style={{color: GOLD}}>Mozilla Firefox</a> o Apple Safari.
+          </p>
+        </div>
+        <FlexCard className="BNSImageContainer">
+          <img src={mozillaFirefoxLogo} className="BNSBrowsersImage" alt="Navegador Mozilla Firefox"></img>
+          <img src={appleSafariLogo} className="BNSBrowsersImage" alt="Navegador Apple Safari"></img>
+          <img src={googleChromeLogo} className="BNSBrowsersImage" alt="Navegador Google Chrome"></img>
+        </FlexCard>
+      </FlexCard>
+    </FlexContainer>
   );
 }
 
