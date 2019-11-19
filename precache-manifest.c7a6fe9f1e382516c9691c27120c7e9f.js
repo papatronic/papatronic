129ef7b5c983f8dbd15e83e7b7cd076cc7b26afd
René Vidríales Trujillo <rene.vidriales@seistreinta.com>
2019-11-19 07:02:11 -0700

    Updates

---
+++ precache-manifest.c7a6fe9f1e382516c9691c27120c7e9f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e039530f0a7842b703453128409b3f0c",
+    "url": "/papatronic/index.html"
+  },
+  {
+    "revision": "265eadb27245bd04ecb5",
+    "url": "/papatronic/static/css/main.e558c775.chunk.css"
+  },
+  {
+    "revision": "de638b7083d5ca814c11",
+    "url": "/papatronic/static/js/2.e21d3f80.chunk.js"
+  },
+  {
+    "revision": "265eadb27245bd04ecb5",
+    "url": "/papatronic/static/js/main.522ff199.chunk.js"
+  },
+  {
+    "revision": "c252de006d5ce5e54e54",
+    "url": "/papatronic/static/js/runtime~main.30a36329.js"
+  },
+  {
+    "revision": "3a73b6a047cb71720f861bf97563ab87",
+    "url": "/papatronic/static/media/TecNM-ITC-Logo.3a73b6a0.png"
+  },
+  {
+    "revision": "416ee08336d874cf8ce1f63832d7c7ca",
+    "url": "/papatronic/static/media/opensans-regular-webfont.416ee083.svg"
+  },
+  {
+    "revision": "47948a378f0f45cf168ab73d593f98d2",
+    "url": "/papatronic/static/media/opensans-regular-webfont.47948a37.eot"
+  },
+  {
+    "revision": "59bab841f00bf2a346538ac8098f8f93",
+    "url": "/papatronic/static/media/opensans-regular-webfont.59bab841.ttf"
+  },
+  {
+    "revision": "7d8bab2cdcf2a4f9c378282fc7297100",
+    "url": "/papatronic/static/media/opensans-regular-webfont.7d8bab2c.woff2"
+  },
+  {
+    "revision": "cf71304a537e5b0ebbba108ed751c411",
+    "url": "/papatronic/static/media/opensans-regular-webfont.cf71304a.woff"
+  },
+  {
+    "revision": "4c6d5b648af03206c753c38402d96439",
+    "url": "/papatronic/static/media/roboto-regular-webfont.4c6d5b64.woff2"
+  },
+  {
+    "revision": "5dca2d677b602f6dfdf8b02a38171d01",
+    "url": "/papatronic/static/media/roboto-regular-webfont.5dca2d67.svg"
+  },
+  {
+    "revision": "7d3ce443daba56f69ffa1d6e1b168fea",
+    "url": "/papatronic/static/media/roboto-regular-webfont.7d3ce443.woff"
+  },
+  {
+    "revision": "9800f66318220d9e2c3df42897a4a7d0",
+    "url": "/papatronic/static/media/roboto-regular-webfont.9800f663.ttf"
+  },
+  {
+    "revision": "b3f43c2e591118c6a2f2694443746ea8",
+    "url": "/papatronic/static/media/roboto-regular-webfont.b3f43c2e.eot"
+  }
+]);
